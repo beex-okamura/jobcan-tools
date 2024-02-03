35e46e76ba169271a61939e68d4b5d21669999ac
shuji okamura <shuji.okamura@beex-inc.com>
2024-02-03 11:24:33 +0900

    Add ecr

--- terraform/modules/aws/ecr.tf
+++ terraform/modules/aws/ecr.tf
@@ -4,3 +4,10 @@ resource "aws_ecr_repository" "jobcan-scraping-lambda-repo" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository" "jobcan-tools-api-repo" {
+  name = "${var.app_name}-tools-api-${var.env}"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
