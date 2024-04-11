20a2bc460718e9eb2097ebc0033fe54c6ea9ab13
shuji okamura <shuji.okamura@beex-inc.com>
2024-04-11 09:25:56 +0900

    Add slack notification

--- functions/scraping-lambda/build.ts
+++ functions/scraping-lambda/build.ts
@@ -12,4 +12,5 @@ esbuild.build({
   banner: { // commonjs用ライブラリをESMプロジェクトでbundleする際に生じることのある問題への対策
     js: 'import { createRequire } from "module"; import url from "url"; const require = createRequire(import.meta.url); const __filename = url.fileURLToPath(import.meta.url); const __dirname = url.fileURLToPath(new URL(".", import.meta.url));',
   },
+  sourcemap: "inline"
 })
