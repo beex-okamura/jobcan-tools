b3e8bac41c51748e4c7552aaceaa54dd2e3228d6
shuji okamura <shuji.okamura@beex-inc.com>
2024-04-12 17:34:40 +0900

    fix method

--- functions/scraping-lambda/src/slack/notification.ts
+++ functions/scraping-lambda/src/slack/notification.ts
@@ -3,6 +3,7 @@ import { getSecretsValues } from '../lib/asm.ts';
 export const sendSlackNotification = async (message: string) => {
   const { JOBCAN_TOOL_API_ENDPOINT, SLACK_VERIFICATION_TOKEN } = await getSecretsValues();
   await fetch(`${JOBCAN_TOOL_API_ENDPOINT}/slack/message`, {
+    method: 'POST',
     body: JSON.stringify({
       token: SLACK_VERIFICATION_TOKEN,
     })
