d07c54d921163817fbadf96fae79e6116f41035b
shuji okamura <shuji.okamura@beex-inc.com>
2024-04-18 10:15:18 +0900

    content-typeをapplication/jsonに変更

--- functions/scraping-lambda/src/slack/notification.ts
+++ functions/scraping-lambda/src/slack/notification.ts
@@ -1,13 +1,20 @@
-import { getSecretsValues } from '../lib/asm.ts';
+import { getSecretsValues } from "../lib/asm.ts";
 
-export const sendSlackNotification = async (channel: string, message: string) => {
-  const { JOBCAN_TOOL_API_ENDPOINT, SLACK_VERIFICATION_TOKEN } = await getSecretsValues();
+export const sendSlackNotification = async (
+  channel: string,
+  message: string,
+) => {
+  const { JOBCAN_TOOL_API_ENDPOINT, SLACK_VERIFICATION_TOKEN } =
+    await getSecretsValues();
   await fetch(`${JOBCAN_TOOL_API_ENDPOINT}/slack/message`, {
-    method: 'POST',
+    headers: {
+      "content-type": "application/json",
+    },
+    method: "POST",
     body: JSON.stringify({
       token: SLACK_VERIFICATION_TOKEN,
       text: message,
       channel,
-    })
-  })
-}
+    }),
+  });
+};
