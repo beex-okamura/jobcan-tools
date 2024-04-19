7d8947c8d5ed05f01e72bcb7a7fe8a970ed98e70
shuji okamura <shuji.okamura@beex-inc.com>
2024-04-19 09:12:53 +0900

    スペルミス修正

--- functions/api/dynamodb/users.py
+++ functions/api/dynamodb/users.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+
+from pynamodb.attributes import ListAttribute, UnicodeAttribute, UTCDateTimeAttribute
 from pynamodb.models import Model
-from pynamodb.attributes import UnicodeAttribute, UTCDateTimeAttribute, ListAttribute
 
 from lib.dynamodb import DynamoDBMeta
 from lib.environment import get_environment
@@ -15,6 +16,6 @@ class UserModel(Model):
     user_id = UnicodeAttribute(hash_key=True)
     jobcan_user_id = UnicodeAttribute()
     jobcan_password = UnicodeAttribute()
-    slack_acess_token = UnicodeAttribute()
+    slack_access_token = UnicodeAttribute()
     send_punch_channel = ListAttribute(of=UnicodeAttribute)
     updated_dt = UTCDateTimeAttribute(default=datetime.now, null=False)
--- functions/api/schemas/users.py
+++ functions/api/schemas/users.py
@@ -5,5 +5,5 @@ class UserInfo(BaseModel):
     user_id: str
     jobcan_user_id: str
     jobcan_password: str
-    slack_acess_token: str
+    slack_access_token: str
     send_punch_channel: list[str] | None
